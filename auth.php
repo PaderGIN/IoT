fb52358f09a4ed5746e1002acb4dace58647490c
Gleb Padergin <glebpad@yandex.ru>
2022-12-01 17:17:51 +0300

    Auth final

--- auth.php
+++ auth.php
@@ -1,29 +1,27 @@
 <!DOCTYPE HTML>
 <html id="App_interface">
-<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>MyApp</title>
     <a href="index.php"> Назад</a> <br>
 </head>
 <body>
 <?php
-require "dbconnect.php";
+include "dbconnect.php";
 $id = $_POST['id'];
-$query = "SELECT device_login FROM device_table WHERE device_id = '$id';";
-$result = pg_query($conn, $query);
+$query = "SELECT device_login FROM device_table WHERE device_id = '$id'";
+$result = pg_query($link, $query);
 $row = pg_fetch_array($result);
 ?>
 
-<form action = 'test.php' method = 'post'>
+<form action="test.php?id=<?php echo $id ?>" method='post'>
     <p><b>Авторизация</b></p>
 
-    <input type="text" maxlength="30" size="40" name="login" placeholder="Введите логин " value = <?php echo $row['DEVIСE_LOGIN'] ?>><br>
+    <input type="text" maxlength="30" size="40" name="login" placeholder="Введите логин "
+           value= <?php echo $row['device_login'] ?>><br>
 
-    <input type="text" maxlength="30" size="40" name="pass" placeholder="Введите пароль" ><br>
+    <input type="text" maxlength="30" size="40" name="pass" placeholder="Введите пароль"><br>
 
     <input type="submit" placeholder="Авторизоваться">
 </form>
-
-
-
-
 </body>
--- test.php
+++ test.php
@@ -1,30 +1,58 @@
 <?php
+
 include "dbconnect.php";
 $login = "";
 $pass = "";
-
+$id = $_GET['id'];
 $login = $_POST['login'];
 $pass = $_POST['pass'];
 $passHash = md5($pass);
-$query = "SELECT DEVIСE_LOGIN FROM `DEVICE_TABLE` WHERE DEVIСE_LOGIN = '$login' AND DEVICE_HASH = '$passHash'";
-$result = mysqli_query($conn, $query);
-
+$query = "SELECT device_login FROM device_table WHERE device_id = '$id' AND device_login = '$login' AND device_hash = '$passHash'";
+$result = pg_query($link, $query);
+$row = pg_fetch_row($result);
 if ($pass != "" && $login != ""){
-    while($row = mysqli_fetch_assoc($result)){
 
+    if (is_countable($row) && count($row) > 0){
+        //header('x-data: '.$id);
+        //header('Location: status.php');
+        RedirectWithMethodPost("./status.php?id=$id");
 
-        if (count($row) == 0){
-            echo '1';
-            echo "<br> <div class = 'error'> Неправильно введен пароль или логин от устройства</div>";
-        }
-        else{
-            echo '2';
-            header('Location: status.php');
-            echo 'вы авторизованы';
-        }
-        echo '3';
+    }
+    else{
+        echo "<br> Неправильно введен пароль или логин от устройства";
     }
 }else{
-    echo "<br> <div class = 'error'>Заполните все поля</div>";
+    echo "<br> Заполните все поля";
+}
+
+function RedirectWithMethodPost($dest){
+    $url = $params = '';
+    if( strpos($dest,'?') ) { list($url,$params) = explode('?',$dest,2); }
+    else { $url = $dest; }
+    echo "<form id='the-form' 
+            method='post' 
+            enctype='multipart/form-data' 
+            action='$url'>\n";
+    foreach( explode('&',$params) as $kv )
+    {
+        if( strpos($kv,'=') === false ) { continue; }
+        list($k,$v) = explode('=',$kv,2);
+        echo "<input type='hidden' name='$k' value='$v'>\n";
+    }
+    echo <<<ENDOFFORM
+            <p id="the-button" style="display:none;">
+            Click the button if page doesn't redirect within 3 seconds.
+            <br><input type="submit" value="Click this button">
+            </p>
+            </form>
+            <script type="text/javascript">
+            function DisplayButton()
+            {
+            document.getElementById("the-button").style.display="block";
+            }
+            setTimeout(DisplayButton,3000);
+            document.getElementById("the-form").submit();
+            </script>
+            ENDOFFORM;
 }
 ?>
